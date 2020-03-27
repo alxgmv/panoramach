118b60d712fad53a2c040df95b513f642106f45b
alxgmv <alxgmv@gmail.com>
2020-03-27 17:03:05 +0100

    1st

---
+++ data.js
@@ -0,0 +1,536 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--rez-de-chausse-1",
+      "name": "Erdgeschoss 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.3041215741818739,
+        "pitch": 0.031108564787725612,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.37022342765800254,
+          "pitch": 0.010025836944608457,
+          "rotation": 0,
+          "target": "1--rez-de-chausse-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0921902326176482,
+          "pitch": -0.02517544682530115,
+          "title": "Bücherregal GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/rs4u1BEqgh\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 2.4586407069818996,
+          "pitch": -0.0923233784791222,
+          "title": "Barhocker CHAYR",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-chayr/ck0a2AZVR\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 2.2839271934391085,
+          "pitch": 0.004495057243989464,
+          "title": "Stuhl CHAYR",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-chayr/Fi66HIIHV\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 1.8284672385636211,
+          "pitch": 0.042186338775733745,
+          "title": "Sideboard GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/L1y-GIg27\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 1.7937142516415445,
+          "pitch": 0.5884328382719222,
+          "title": "Sessel PYLLOW",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/sofa-pyllow/42cmuZ_ZY\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 1.1870881040615924,
+          "pitch": 0.3727631940095115,
+          "title": "Beistelltisch SYDE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/beistelltisch/zHQbiPs0X\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.876283964342953,
+          "pitch": 0.8602478980690016,
+          "title": "Beistelltisch SYDE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/beistelltisch/F-X4sylAj\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.1553164180159925,
+          "pitch": 0.3179984805920011,
+          "title": "Ecksofa TYME",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/sofa-tyme//rVXW6qRUb\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -1.4512188196840157,
+          "pitch": 0.4929100991975748,
+          "title": "Ottomane TYME",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/sofa-tyme/VsWfxJarab\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.9844304874310446,
+          "pitch": 0.3703408283230587,
+          "title": "Beistelltisch SYDE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/beistelltisch/ziWipbYAv\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.7971601511693596,
+          "pitch": 0.14888141343105943,
+          "title": "Rollcontainer GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/umWAmuaxr\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.48232918167616745,
+          "pitch": -0.02830063245821357,
+          "title": "Konsolentisch SYDE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/beistelltisch/z-KKS6Nye\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.2695068933317337,
+          "pitch": 0.027894038440964053,
+          "title": "Stuhl CHAYR",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-chayr/QHhSUcMWE\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.03415685189738582,
+          "pitch": 0.12408165365331314,
+          "title": "Sideboard GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/CMDELhfKi\">Im Konfigurator öffnen</a>"
+        }
+      ]
+    },
+    {
+      "id": "1--rez-de-chausse-2",
+      "name": "Erdgeschoss 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09223155893822899,
+          "pitch": 0.03718758220168539,
+          "rotation": 0,
+          "target": "0--rez-de-chausse-1"
+        },
+        {
+          "yaw": -3.0887168014657433,
+          "pitch": 0.11530988800252828,
+          "rotation": 0,
+          "target": "4-deuxime-tage-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1890496850086798,
+          "pitch": 0.2221233339381108,
+          "title": "Esstisch TYMBER",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/tisch/GHRtD0j7k\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.41152570089376184,
+          "pitch": 0.3587420781102537,
+          "title": "Stuhl PRYME",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-pryme/QitqbTFIx\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.09709675990220568,
+          "pitch": 0.15770782945140915,
+          "title": "Stuhl STYNG",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-styng/F-8svFhHV\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.052210899737751504,
+          "pitch": 0.17524635529432153,
+          "title": "Stuhl STYNG",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-styng/Q-rxICFWd\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.09260312918033442,
+          "pitch": 0.4557819869954933,
+          "title": "Stuhl PRYME",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-pryme/QH_ClcngE\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.41572416858793204,
+          "pitch": 0.38169868516570205,
+          "title": "Stuhl PRYME",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-pryme/QwhfcGQWK\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 1.1971160605951994,
+          "pitch": 0.2427915478812821,
+          "title": "Schreibtisch TYMBER",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/tisch/StROpQSt6\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.9353997195901051,
+          "pitch": 0.40682660676628757,
+          "title": "Stuhl PRYME",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-pryme/e_TnPxjef\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 2.3515358855710673,
+          "pitch": 0.22469463990405458,
+          "title": "Regal GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/h-phR-5-q\">Im Konfigurator öffnenr</a>"
+        },
+        {
+          "yaw": -1.0393665282253082,
+          "pitch": 0.11134861061340828,
+          "title": "Rollcontainer GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/Q-VgGrsMZ\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -1.3984853213006971,
+          "pitch": 0.0636172775567001,
+          "title": "Stuhl STYNG",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-styng/Q-rxICFWd\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.7188716354417188,
+          "pitch": 0.1998146598981787,
+          "title": "Sideboard GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/CMDELhfKi\">Im Konfigurator öffnen</a>"
+        }
+      ]
+    },
+    {
+      "id": "2-deuxime-tage-3",
+      "name": "Erster Stock 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18122418511981664,
+          "pitch": 0.064993444722095,
+          "rotation": 0,
+          "target": "3-deuxime-tage-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.49912919113365106,
+          "pitch": 0.21744586970154955,
+          "title": "Tisch TYMBER",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/tisch/SqdBul2gv\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.44144326676837053,
+          "pitch": 0.41614914472225983,
+          "title": "Beistelltisch SYDE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/beistelltisch/FHWEev0nj\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -1.117805072162895,
+          "pitch": 0.271077666826379,
+          "title": "Schreibtisch TYMBER",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/tisch/pHIi1wYEE\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.28429640138986123,
+          "pitch": 0.5463667863361117,
+          "title": "Schlafsofa TYME",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/sofa-tyme/7S7NX4mS3\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 2.4136306750273535,
+          "pitch": 0.45710627387685676,
+          "title": "Couchtisch ECLYPSE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/couchtisch/QiW1dTc5\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -2.269096888484251,
+          "pitch": 0.3084386159670096,
+          "title": "Kleiderschrank LYNE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/kleiderschrank/H4sUC94N\">Im Konfigurator öffnen</a>"
+        }
+      ]
+    },
+    {
+      "id": "3-deuxime-tage-2",
+      "name": "Erster Stock 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.014620116559505902,
+          "pitch": 0.03301679936498303,
+          "rotation": 0,
+          "target": "2-deuxime-tage-3"
+        },
+        {
+          "yaw": 2.2821243997639478,
+          "pitch": 0.09701778634264713,
+          "rotation": 0,
+          "target": "4-deuxime-tage-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9957554626553833,
+          "pitch": 0.29976432058616354,
+          "title": "Konsolentisch SYDE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/beistelltisch/F-oXDOZRv\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 1.6631139591294488,
+          "pitch": 0.361123758399188,
+          "title": "Schreibtisch TYMBER",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/tisch/SIdnWoOE1\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 1.1127586552926196,
+          "pitch": 0.4305977166134838,
+          "title": "Rollcontainer GRYD",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/regal/zHV1Z5Nav\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -1.6826345783849632,
+          "pitch": 0.443646965375704,
+          "title": "Schlafsofa TYME",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/sofa-tyme/pHdaaMuFE\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.9624058060027991,
+          "pitch": 0.35755381314202417,
+          "title": "Beistelltisch SYDE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/beistelltisch/FwfQIr2vZ\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.07527138948824863,
+          "pitch": 0.22143683720155494,
+          "title": "Stuhl STYNG",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-styng/pwxef-nqd\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.2060116452478553,
+          "pitch": 0.20718625514057898,
+          "title": "Stuhl STYNG",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-styng/p-IKUyBrx\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.1509730187990197,
+          "pitch": 0.5356608397324241,
+          "title": "Stuhl STYNG",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-styng/pwWV_4BWd\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.46992825136742766,
+          "pitch": 0.4351910368546932,
+          "title": "Stuhl CHAYR",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/stuhl-chayr/5ma3Qd2a1\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.18589575153439952,
+          "pitch": 0.36376225578148613,
+          "title": "Esstisch TYMBER",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/tisch/RWU3Q_8PX\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.33939114631396095,
+          "pitch": 0.25136310177017585,
+          "title": "Beistelltisch SYDE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/beistelltisch/FHWEev0nj\">Im Konfigurator öffnen</a>"
+        }
+      ]
+    },
+    {
+      "id": "4-deuxime-tage-1",
+      "name": "Erster Stock 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.48100463600884247,
+          "pitch": 0.015335632472147864,
+          "rotation": 0,
+          "target": "3-deuxime-tage-2"
+        },
+        {
+          "yaw": 2.2176035454024134,
+          "pitch": 0.1409539658104606,
+          "rotation": 0,
+          "target": "1--rez-de-chausse-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.82315424746571,
+          "pitch": 0.5261510887168761,
+          "title": "Schlafsofa PYLLOW",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/sofa-pyllow/Pjj_D3Ztp\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -2.187980967602666,
+          "pitch": 0.4620502339650159,
+          "title": "Couchtisch ECLYPSE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/couchtisch/pwC-lbPyj\">Im Konfigurator öffnenr</a>"
+        },
+        {
+          "yaw": -1.7655427675548019,
+          "pitch": 0.5208924051354451,
+          "title": "Couchtisch ECLYPSE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/couchtisch/zi-9JjFM\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -1.1948684497098743,
+          "pitch": 0.361376422950066,
+          "title": "Sessel TYME",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/sofa-tyme/vemMXSNO5\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": -0.09027797517503622,
+          "pitch": 0.32213452908433027,
+          "title": "	Ecksofa PYLLOW",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/sofa-pyllow/Z6DyPagqm\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.8431625181208275,
+          "pitch": 0.6338464956988545,
+          "title": "Couchtisch ECLYPSE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/couchtisch/L-nuHjCme\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.31753349206172743,
+          "pitch": 0.7292576335080039,
+          "title": "Couchtisch ECLYPSE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/couchtisch/zHVmLldf\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 0.6405247601849116,
+          "pitch": 0.3827041817217367,
+          "title": "Couchtisch ECLYPSE",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/couchtisch/2wCRAK20c\">Im Konfigurator öffnen</a>"
+        },
+        {
+          "yaw": 1.339109617350454,
+          "pitch": 0.6236603487063892,
+          "title": "Ottomane PYLLOW",
+          "text": "<a target='_blank' href=\"https://de.mycs.com/sofa-pyllow/a7RaJOUiF\">Im Konfigurator öffnen</a>"
+        }
+      ]
+    }
+  ],
+  "name": "Showroom",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
